aba996173956b7e64ac656e5f48c8c8a6f02c39f
Abaunza Garcia Juan Jesus (BGSW EBS5.2-SX) <abj1ga@bosch.com>
2025-09-10 10:25:53 -0600

    change in PairNumberFromColor

--- TelCo.ColorCoder/Services/PairNumberFromColor.cs
+++ TelCo.ColorCoder/Services/PairNumberFromColor.cs
@@ -14,26 +14,11 @@ namespace TelCo.ColorCoder.Services
         internal static int GetPairNumberFromColor(ColorPair pair, Color[] colorMapMajor, Color[] colorMapMinor)
         {
             // Find the major color in the array and get the index
-            int majorIndex = -1;
-            for (int i = 0; i < colorMapMajor.Length; i++)
-            {
-                if (colorMapMajor[i] == pair.majorColor)
-                {
-                    majorIndex = i;
-                    break;
-                }
-            }
+            int majorIndex = FindColor(pair.majorColor, colorMapMajor);
 
             // Find the minor color in the array and get the index
-            int minorIndex = -1;
-            for (int i = 0; i < colorMapMinor.Length; i++)
-            {
-                if (colorMapMinor[i] == pair.minorColor)
-                {
-                    minorIndex = i;
-                    break;
-                }
-            }
+            int minorIndex = FindColor(pair.minorColor, colorMapMinor);
+
             // If colors can not be found throw an exception
             if (majorIndex == -1 || minorIndex == -1)
             {
@@ -44,6 +29,21 @@ namespace TelCo.ColorCoder.Services
             // Compute pair number and Return  
             // (Note: +1 in compute is because pair number is 1 based, not zero)
             return (majorIndex * colorMapMinor.Length) + (minorIndex + 1);
+        }
+
+        private static int FindColor(Color color, Color[] colorMap)
+        {
+            int index = -1;
+            for (int i = 0; i < colorMap.Length; i++)
+            {
+                if (colorMap[i] == color)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
         }
     }
 }
